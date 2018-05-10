e4b64a94fd24cfbe1b69b0a56fa211e4f08da676
DanielSilvaol <odsdaniel99@gmail.com>
2018-05-10 00:24:15 -0300

    Aula09

---
+++ eclipse-workspace/Aula09/src/command/AlterarPais.java
@@ -0,0 +1,78 @@
+package command;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pacote.Pais;
+import service.PaisService;
+
+public class AlterarPais implements Command {
+
+	@Override
+	public void executar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String pId = request.getParameter("id");
+		String pNome = request.getParameter("nome");
+		String Populacao = request.getParameter("populacao");
+		String Area = request.getParameter("area");
+
+		int id = -1;
+		long pPopulacao = -1;
+		double pArea = -1;
+		try {
+			id = Integer.parseInt(pId);
+			pPopulacao = Long.valueOf(Populacao);
+			pArea = Double.valueOf(Area);
+		} catch (NumberFormatException e) {
+
+		}
+
+		Pais pais = new Pais();
+		pais.setPopulacao(pPopulacao);
+		pais.setArea(pArea);
+		
+		PaisService cs = new PaisService();
+		cs.criar(pais);
+		pais = cs.carregar(pais.getId());
+		request.setAttribute("pais", pais);
+		RequestDispatcher view = request.getRequestDispatcher("Pais.jsp");
+		
+		pais.setId(id);
+		pais.setNome(pNome);
+		pais.setPopulacao(pPopulacao);
+		pais.setArea(pArea);
+		PaisService pai = new PaisService();
+		HttpSession session = request.getSession();
+		
+		pai.atualizar(pais);
+		@SuppressWarnings("unchecked")
+		ArrayList<Pais> lista = (ArrayList<Pais>) session.getAttribute("lista");
+		int pos = busca(pais, lista);
+		lista.remove(pos);
+		lista.add(pos, pais);
+		session.setAttribute("lista", lista);
+		request.setAttribute("pais", pais);
+		view = request.getRequestDispatcher("VisualizarPais.jsp");
+		view.forward(request, response);
+
+	
+	}
+	public int busca(Pais pais, ArrayList<Pais> lista) {
+		Pais to;
+		for (int i = 0; i < lista.size(); i++) {
+			to = lista.get(i);
+			if (to.getId() == pais.getId()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+
+}
---
+++ eclipse-workspace/Aula09/src/command/EditarPais.java
@@ -0,0 +1,84 @@
+package command;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pacote.Pais;
+import service.PaisService;
+
+public class EditarPais implements Command {
+
+	@Override
+	public void executar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String pAcao = request.getParameter("acao");
+		String pId = request.getParameter("id");
+		String pNome = request.getParameter("nome");
+		String Populacao = request.getParameter("populacao");
+		String Area = request.getParameter("area");
+
+		int id = -1;
+		long pPopulacao = -1;
+		double pArea = -1;
+		try {
+			id = Integer.parseInt(pId);
+			pPopulacao = Long.valueOf(Populacao);
+			pArea = Double.valueOf(Area);
+		} catch (NumberFormatException e) {
+
+		}
+
+		Pais pais = new Pais();
+		pais.setPopulacao(pPopulacao);
+		pais.setArea(pArea);
+
+		if (pAcao.equals("Criar")) {
+			pPopulacao = Long.valueOf(Populacao);
+			pArea = Double.valueOf(Area);
+
+			pais.setId(id);
+			pais.setNome(pNome);
+			pais.setPopulacao(pPopulacao);
+			pais.setArea(pArea);
+		}
+		// instanciar o service
+
+		PaisService cs = new PaisService();
+		cs.criar(pais);
+		pais = cs.carregar(pais.getId());
+		request.setAttribute("pais", pais);
+		RequestDispatcher view = request.getRequestDispatcher("Pais.jsp");
+		
+		pais.setId(id);
+		pais.setNome(pNome);
+		pais.setPopulacao(pPopulacao);
+		pais.setArea(pArea);
+		PaisService pai = new PaisService();
+		HttpSession session = request.getSession();
+		
+		pais = pai.carregar(pais.getId());
+		request.setAttribute("pais", pais);
+		view = request.getRequestDispatcher("AlterarPais.jsp");
+		view.forward(request, response);
+
+	
+	}
+	public int busca(Pais pais, ArrayList<Pais> lista) {
+		Pais to;
+		for (int i = 0; i < lista.size(); i++) {
+			to = lista.get(i);
+			if (to.getId() == pais.getId()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
